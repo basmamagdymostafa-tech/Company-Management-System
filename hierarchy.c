ffebd326a5d9b0baf23e275db6209c3ea7c36e00
Abdullah7173 <abdullahyoussef789@gmail.com>
2026-08-13 21:05:55 +0300

    Add functions to view direct reports and hierarchy
    Implemented functions to view direct reports and employee hierarchy.

--- hierarchy.c
+++ hierarchy.c
@@ -1,10 +1,75 @@
-# include "hierarchy.h"
+#include <stdio.h>
+#include <string.h>
+#include "Config.h"
+#include "hierarchy.h"
+
 void viewDirectReports(void)
 {
-    /* To be implemented */
+    int target_id;
+    int i;
+    int found_anyone = 0;
+
+    printf("\n--- View Direct Reports ---\n");
+    printf("Enter the Manager's Employee ID: ");
+    scanf("%d", &target_id);
+    printf("\nEmployees who report directly to Manager ID %d:\n", target_id);
+    printf("ID \t Name \t\t Job Title\n");
+    printf("----------------------------------------------------\n");
+    for (i = 0; i < total_employees; i++) {
+        
+        if (employee_list[i].managerID == target_id) {
+            printf("%d \t %s \t %s\n", 
+                   employee_list[i].employeeID, 
+                   employee_list[i].fullName, 
+                   employee_list[i].jobTitle);
+            found_anyone = 1; 
+        }
+    }
+
+    if (found_anyone == 0) {
+        printf("This employee has no direct subordinates.\n");
+    }
 }
+
+void printSubordinates(int manager_id, int level)
+{
+    int i;
+    int j;
+
+    for (i = 0; i < total_employees; i++) {
+        
+        if (employee_list[i].managerID == manager_id) {
+            for (j = 0; j < level; j++) {
+                printf("    ");
+            }
+            printf("|-- %s (ID: %d)\n", employee_list[i].fullName, employee_list[i].employeeID);
+            printSubordinates(employee_list[i].employeeID, level + 1);
+        }
+    }
+}
+
+
 
 void viewEmployeeHierarchy(void)
 {
-    /* To be implemented */
+    int target_id;
+    int i;
+    int found_manager = 0;
+    printf("\n--- View Full Employee Hierarchy ---\n");
+    printf("Enter the Top Manager's Employee ID: ");
+    scanf("%d", &target_id);
+    for (i = 0; i < total_employees; i++) {
+        if (employee_list[i].employeeID == target_id) {
+            printf("\nHierarchy for: %s (Department ID: %d)\n", 
+                   employee_list[i].fullName, 
+                   employee_list[i].departmentID);
+            found_manager = 1;
+        }
+    }
+
+    if (found_manager == 0) {
+        printf("Error: Employee ID %d not found in the system.\n", target_id);
+        return;
+    }
+    printSubordinates(target_id, 1);
 }
