13931c82d775c545bc1c70e5624029c6f07eb2d7
Basma Magdy <basmamagdymostafa@gmail.com>
2026-09-14 22:59:55 +0300

    Update Storage.h

--- Storage.h
+++ Storage.h
@@ -1,13 +1,18 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-int saveEmployee(void);
-int loadEmployee(void);
+#include "config.h"
 
-int saveCustomers(void);
-int loadCustomers(void);
+s32 saveEmployee(void);
+s32 loadEmployee(void);
 
-int saveDepartments(void);
-int loadDepartments(void);
+s32 saveCustomers(void);
+s32 loadCustomers(void);
 
-#endif // STORAGE_H
+s32 saveDepartments(void);
+s32 loadDepartments(void);
+
+s32 saveNextIDs(void);
+s32 loadNextIDs(void);
+
+#endif /* STORAGE_H */
