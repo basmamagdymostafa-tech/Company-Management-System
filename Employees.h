fd8fdf2caeb822764fdc51de23f8de7a22f7e886
Basma Magdy <basmamagdymostafa@gmail.com>
2026-09-14 22:59:08 +0300

    Update Employees.h

--- Employees.h
+++ Employees.h
@@ -1,18 +1,12 @@
 #ifndef EMPLOYEES_H
 #define EMPLOYEES_H
 
-#include "Config.h"
-struct employee employees[100];
-void clearInputBuffer(void);
-int readString(const char *, char *, int);
-int readInt(const char *, int *);
-int validatePhone(const char *);
-int validateEmail(const char *);
-int validateEmployeeID(int , int);
-int validateDepartmentID(int );
-int validateManagerID(int , int , int );
-int checkCircular(int , int , int );
-int validateDate(int, int, int );
+#include "config.h"
+
+extern struct employee employees[MAX_EMPLOYEES];
+extern s32 nextEmployeeID;
+
 void addEmployee(void);
 void viewEmployee(void);
-#endif // EMPLOYEES_H
+
+#endif /* EMPLOYEES_H */
